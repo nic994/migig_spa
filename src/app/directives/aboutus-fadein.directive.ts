30a10a8a0ea82f6e0286ffa6b57d285a164257ae
nic994 <nic4e4943@gmail.com>
2025-03-09 13:20:41 +0800

    home page parallax fx

--- src/app/directives/aboutus-fadein.directive.ts
+++ src/app/directives/aboutus-fadein.directive.ts
@@ -1,14 +1,13 @@
 import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Directive({
   selector: '[appAboutusFadein]',
   standalone: true,
 })
 export class AboutusFadeinDirective {
-  fadedIn = false;
-  firstCardScrollPosition = 0; // Initialize with the default value
+  fadedInAboutUs = false;
+  fadedInContactUs = false;
+  fadedInService = false;
 
   constructor(private el: ElementRef, private renderer: Renderer2) {}
   ngOnInit() {
@@ -18,16 +17,52 @@ export class AboutusFadeinDirective {
   onWindowScroll() {
     const scrollPosition = window.scrollY;
     const screenWidth = window.innerWidth;
-    const triggerPosition = screenWidth > 2900 ? 2005 : 0;
+
+    const triggerPositionService = screenWidth > 700 ? 800 : 1200;
+    const triggerPositionContactUs = screenWidth > 700 ? 1106 : 2100;
+    const triggerPositionAboutUs = screenWidth > 700 ? 1800 : 3012;
+
     console.log(scrollPosition);
-    if (scrollPosition >= triggerPosition && !this.fadedIn) {
-      this.fadeInCard(this.el.nativeElement, 10); // Add a delay of 10ms
-      this.fadedIn = true;
+    const serviceElement = document.getElementById('services');
+    const contactUsElement = document.getElementById('contact-us');
+    const aboutUsElement = document.getElementById('about-us');
+
+    // Fade in Services
+
+    if (
+      serviceElement &&
+      scrollPosition >= triggerPositionService &&
+      !this.fadedInService
+    ) {
+      this.fadeInCard(serviceElement, 10);
+      this.fadedInService = true;
+    }
+    // Fade in Contact Us
+    if (
+      contactUsElement &&
+      scrollPosition >= triggerPositionContactUs &&
+      !this.fadedInContactUs
+    ) {
+      this.fadeInCard(contactUsElement, 10);
+      this.fadedInContactUs = true;
+    }
+
+    // Fade in About Us
+
+    if (
+      aboutUsElement &&
+      scrollPosition >= triggerPositionAboutUs &&
+      !this.fadedInAboutUs
+    ) {
+      this.fadeInCard(aboutUsElement, 10);
+      this.fadedInAboutUs = true;
     }
   }
-  private fadeInCard(cardElement: HTMLElement | null, delay: number) {
+
+  private fadeInCard(cardElement: HTMLElement, delay: number) {
     if (cardElement) {
       setTimeout(() => {
+        console.log(cardElement.id);
         this.renderer.addClass(cardElement, 'fade-in');
       }, delay);
     }
